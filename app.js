562416fd30b35ff1cb9485e37ca4a4aa7025c314
Maud de Vries <F3PiXnl@gmail.com>
2018-04-16 21:05:53 +0200

    save submitted idea to database

--- app.js
+++ app.js
@@ -47,8 +47,16 @@ app.post('/ideas', (req, res) => {
       details: req.body.details
     });
   } else {
-    res.send("Form submitted");
-  }
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    }
+    new Idea(newUser)
+      .save() //or just pass in req.body; does this look like premature optimization?
+      .then(idea => {
+        res.redirect('/ideas');
+      })
+    }
 }); 
 
 const port = 5000;
